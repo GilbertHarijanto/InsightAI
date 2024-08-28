f3166d9350587b21cf4d22cd8154e47f1bc5b584
Gilbert Harijanto <88890882+GilbertHarijanto@users.noreply.github.com>
2024-08-28 07:41:43 +0700

    Delete src directory

--- src/pdfWorker.js
+++
@@ -1,5 +0,0 @@
-// src/pdfWorker.js
-import * as pdfjsLib from 'pdfjs-dist';
-
-// Ensure that the workerSrc is set to the appropriate path.
-pdfjsLib.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.js`;
